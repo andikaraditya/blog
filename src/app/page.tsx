f37ffc4f26b76d92dec706b509a740ec544ba0fc
andika <andikaraditya1@gmail.com>
2024-05-31 20:42:04 +0700

    add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import getPostMetadata from "./components/getPostMetadata"
 export default function Home() {
   const titles = getPostMetadata()
   return (
-    <main className="bg-gradient-to-b from-gray-100 to-white">
+    <main className="bg-gray-100 dark:bg-[#1F1F1F] min-h-[100vh]">
       <div className="max-md:w-[85%] flex flex-col gap-3 w-[700px] mx-auto">
-        <h1 className="text-4xl md:text-6xl font-bold text-slate-800 my-11 text-center">
+        <h1 className="text-4xl md:text-6xl font-bold text-slate-900 my-11 text-center dark:text-gray-300">
           Lightstation Blog
         </h1>
         {titles.map((el, index) => {
@@ -24,9 +24,11 @@ interface TitleProps {
 
 const BlogTitle: React.FC<TitleProps> = ({ title, slug }) => {
   return (
-    <div className="bg-slate-100 pt-3 pb-1 px-3 border-b-[3px] border-slate-800">
+    <div className="pt-3 pb-1 px-3 border-b-[3px] border-slate-800 dark:bg-[#303134] dark:border-gray-300">
       <Link href={`/posts/${slug}`}>
-        <h2 className="text-2xl  md:text-3xl font-semibold line-clamp-1">{title}</h2>
+        <h2 className="text-2xl  md:text-3xl font-semibold line-clamp-1 dark:text-gray-300">
+          {title}
+        </h2>
       </Link>
     </div>
   )
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -48,17 +48,19 @@ function BlogPage(props: any) {
   const slug = props.params.slug
   const post = getPostContent(slug)
   return (
-    <main className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="max-lg:w-[85%] w-[60%] mx-auto pt-10">
-        <Link className="text-2xl underline" href={"/"}>
+    <main className="bg-gray-100 dark:bg-[#1F1F1F] min-h-[100vh]">
+      <div className="w-[85%] xl:w-[60%] mx-auto pt-10">
+        <Link className="text-2xl underline dark:text-gray-300" href={"/"}>
           Home
         </Link>
-        <h1 className="text-4xl md:text-6xl font-bold text-slate-800 my-4 text-center">
+        <h1 className="lg:w-[800px] mx-auto text-4xl md:text-6xl font-bold text-slate-800 my-4 text-center dark:text-gray-100">
           {post.title}
         </h1>
-        <p className="text-center mx-auto">Posted on {new Date(post.date).toDateString()}</p>
+        <p className="text-center mx-auto dark:text-gray-300 ">
+          Posted on {new Date(post.date).toDateString()}
+        </p>
       </div>
-      <div className="max-lg:w-[85%] prose prose-base md:prose-xl prose-h2:text-slate-800 prose-p:text-justify mx-auto">
+      <div className="max-lg:w-[85%] prose prose-base md:prose-xl prose-h2:text-slate-800 prose-p:text-justify mx-auto dark:text-gray-300 dark:prose-h2:dark:text-gray-100">
         <Markdown>{post.content}</Markdown>
       </div>
     </main>
