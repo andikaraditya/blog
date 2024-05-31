4b51fb3111b088fa5bffc2efcbfe5dbb57f1068a
andika <andikaraditya1@gmail.com>
2024-05-31 20:15:33 +0700

    add responsive style and posts metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   const titles = getPostMetadata()
   return (
     <main className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="flex flex-col gap-3 w-[700px] mx-auto">
-        <h1 className="text-6xl font-bold text-slate-800 my-11 text-center">Lightstation Blog</h1>
+      <div className="max-md:w-[85%] flex flex-col gap-3 w-[700px] mx-auto">
+        <h1 className="text-4xl md:text-6xl font-bold text-slate-800 my-11 text-center">
+          Lightstation Blog
+        </h1>
         {titles.map((el, index) => {
           return <BlogTitle title={el.title} slug={el.slug} key={index} />
         })}
@@ -24,7 +26,7 @@ const BlogTitle: React.FC<TitleProps> = ({ title, slug }) => {
   return (
     <div className="bg-slate-100 pt-3 pb-1 px-3 border-b-[3px] border-slate-800">
       <Link href={`/posts/${slug}`}>
-        <h2 className="text-3xl font-semibold line-clamp-1">{title}</h2>
+        <h2 className="text-2xl  md:text-3xl font-semibold line-clamp-1">{title}</h2>
       </Link>
     </div>
   )
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import getPostMetadata from "@/app/components/getPostMetadata"
 import fs from "fs"
+import type { Metadata, ResolvingMetadata } from "next"
 import matter from "gray-matter"
 import Markdown from "markdown-to-jsx"
 import Link from "next/link"
@@ -26,19 +27,38 @@ export async function generateStaticParams() {
   })
 }
 
+type Props = {
+  params: { slug: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const { slug } = params
+  const folder = "posts/"
+  const file = `${folder}${slug}.md`
+  const content = fs.readFileSync(file, "utf-8")
+  const matterResult = matter(content)
+  return {
+    title: matterResult.data.title,
+    description: matterResult.data.subtitle,
+  }
+}
+
 function BlogPage(props: any) {
   const slug = props.params.slug
   const post = getPostContent(slug)
   return (
     <main className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="w-[800px] mx-auto pt-10">
+      <div className="max-lg:w-[85%] w-[60%] mx-auto pt-10">
         <Link className="text-2xl underline" href={"/"}>
           Home
         </Link>
-        <h1 className="text-6xl font-bold text-slate-800 my-4 text-center">{post.title}</h1>
+        <h1 className="text-4xl md:text-6xl font-bold text-slate-800 my-4 text-center">
+          {post.title}
+        </h1>
         <p className="text-center mx-auto">Posted on {new Date(post.date).toDateString()}</p>
       </div>
-      <div className="prose lg:prose-xl mx-auto">
+      <div className="max-lg:w-[85%] prose prose-base md:prose-xl prose-h2:text-slate-800 prose-p:text-justify mx-auto">
         <Markdown>{post.content}</Markdown>
       </div>
     </main>
